5ce88ba30b032db4142869d22f4384c7c93e77f4
keulma <jjyoung0403@naver.com>
2025-11-19 01:22:05 +0900

    fix:로그인 후 리디렉션 url 수정

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -7,7 +7,7 @@ const googleCallback = (req, res) => {
   // 정상 로그인 시 구글 로그인 페이지에서 다음 페이지로 리디렉션
   if (user.roomId) {
     // 방이 있을 경우 바로 메인 페이지로 이동
-    res.redirect(process.env.FRONTEND_URL + '/main');
+    res.redirect(process.env.FRONTEND_URL + '/dashboard');
   } else {
     // 방이 없을 경우 프로필 설정부터 하도록 이동
     res.redirect(process.env.FRONTEND_URL + '/onboarding/profile');
