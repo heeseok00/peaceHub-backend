353d197f6fc2d3cd75376b5ab22a166b90cac76c
keulma <jjyoung0403@naver.com>
2025-11-25 17:34:53 +0900

    feat: 스케줄러 기능 구현

--- scheduler.js
+++ scheduler.js
@@ -1,17 +1,17 @@
 const cron = require('node-cron'); // 스케줄러
-const allocationService = require('./services/allocation.service');
-const scheduleService = require('./services/schedules.service');
+const allocationService = require('./services/allocation.service'); // 업무 분배 알고리즘
+const scheduleService = require('./services/schedules.service'); // 스케줄 아카이빙, 승격, 복제
 
 const task = cron.schedule('0 2 * * 0', async () => {
   try {
     // 스케줄 아카이빙, 승격, 복사 진행
     await scheduleService.archiveAndCopySchedule();
-    
+
     // 업무 분배
-    await allocationService.taskAllocation();
-    
+    await allocationService.runWeeklyAssignment();
+
   } catch (error) {
-    console.error('error', error);
+    console.error('[Scheduler Error]', error);
   }
 });
 
